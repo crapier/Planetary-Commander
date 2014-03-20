93487d06b9e30b41d49e1e877daa41b2ef4fded8
Clifton <c.rapier93@hotmail.com>
2014-03-19 20:46:43 -0500

    Reorganized and added sever that currently does nothing but http requests

---
+++ server.js
@@ -0,0 +1,45 @@
+// The node.js HTTP server.
+var app = require('http').createServer(handler);
+// The socket.io WebSocket server, running with the node.js server.
+var io = require('socket.io').listen(app);
+// Allows access to local file system.
+var fs = require('fs');
+
+// Listen on a high port.
+var listen_port = 61111;
+app.listen(listen_port);
+console.log('HTTP Server is listening on port: ' + listen_port);
+
+// Handles HTTP requests.
+function handler(request, response) {
+	var path = request.url;
+	switch (path) {
+		case '/':
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.write(fs.readFileSync(__dirname + '/index.html', 'utf8'));
+			response.end();
+			break;
+		default:
+			if (/\.(css)$/.test(path)) {
+				response.writeHead(200, {'Content-Type': 'text/css'});
+				response.write(fs.readFileSync(__dirname + path, 'utf8'));
+				response.end();
+				break;
+			}
+			response.send(404, 'Could not find ' + path);
+			response.end();
+			break;
+	}
+}
+
+// Listen for 'connection'
+io.sockets.on('connection', 
+	function(client) {
+	
+	// Listen for 'disconnect'
+	client.on( 'disconnect', 
+		function() {
+		
+		});
+	});
+
